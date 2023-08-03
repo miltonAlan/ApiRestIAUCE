a2e82861e8c4a16eec65bcb351ebd37560b89fdb
mpaucar <MY_NAME@example.com>
2023-08-02 19:05:06 -0500

    ajustes para correr server en localhost

---
+++ notas.sql
@@ -0,0 +1,57 @@
+ipconfig --> ipv4
+flask run -h 192.168.56.1
+-- instalacion flutter (dart), android studio, etc
+https://docs.flutter.dev/get-started/install
+
+--> Repositorios
+-- API
+https://github.com/miltonAlan/ejemploAPI
+
+-- ejecutar API
+py -m venv .venv
+.venv\scripts\activate
+pip install -r requirements.txt
+flask run
+
+URL's' POSTMAN
+-- LOCALHOST
+http://localhost:5000
+
+-- metodo GET
+http://localhost:5000/datetime
+
+-- se debe escoger la opcion body->from-data-> key: 'img' y escoger la imagen
+-- metodo POST
+http://localhost:5000/invert
+
+-- AZURE
+-- metodo GET
+https://ejemplosimple.azurewebsites.net/datetime
+
+
+-- se debe escoger la opcion body->from-data-> key: 'img' y escoger la imagen
+-- metodo POST
+https://ejemplosimple.azurewebsites.net/invert
+
+https://ejemplosimple.azurewebsites.net
+
+
+-- crear cuenta
+https://portal.azure.com/#home
+
+---------------------------------------------------------------------------------------
+
+-- appMovil
+https://github.com/miltonAlan/movilMineria
+
+-- imports dart
+dart pub get
+
+-- build APK
+  flutter build apk
+
+
+
+
+
+
