df8a6b08a2d7a76c8f2f6bf0773447779b0b1f27
mpaucar <MY_NAME@example.com>
2023-08-22 11:11:02 -0500

    se guarda imagen con cajas y un json con el mismo nombre de la imagen

--- propio/app.py
+++ propio/app.py
@@ -4,35 +4,43 @@ from torchvision.transforms import functional as F
 import cv2
 import matplotlib.pyplot as plt
 import os
+import json
+import numpy as np
 
-def perform_object_detection(img_folder, model_weights_path, show_image=True, save_path=None):
-    # Definir las clases según las clases en tu conjunto de datos
+def convert_to_serializable(obj):
+    if isinstance(obj, np.float32):
+        return float(obj)
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+def save_detection_results_to_json(json_path, json_filename, detection_results):
+    if not json_filename.endswith('.json'):
+        json_filename += '.json'
+    
+    json_file_path = os.path.join(json_path, json_filename)
+    
+    with open(json_file_path, 'w') as json_file:
+        json.dump(detection_results, json_file, default=convert_to_serializable)
+
+    return json_file_path
+
+def perform_object_detection(img_folder, model_weights_path, show_image=True, save_path=None, json_path=None):
     classes = ['rosas', '0', '1', '2']
-
-    # Crear una instancia del modelo con la misma arquitectura
     model = models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=False)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(
         in_features, len(classes))
-
-    # Cargar los pesos del modelo entrenado
     model.load_state_dict(torch.load(model_weights_path,
                           map_location=torch.device('cpu')))
-    
-    model.eval()  # Poner el modelo en modo de evaluación
-
+    model.eval()
     probabilidad = 0.5
+    detection_results = []
 
     for image_filename in os.listdir(img_folder):
         if image_filename.endswith('.jpg') or image_filename.endswith('.png') or image_filename.endswith('.jpeg'):
             image_path = os.path.join(img_folder, image_filename)
-
-            # Preprocesar la imagen
             image = cv2.imread(image_path)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image_tensor = F.to_tensor(image).unsqueeze(
-                0)  # Mantener el tensor para inferencia
-
+            image_tensor = F.to_tensor(image).unsqueeze(0)
             with torch.no_grad():
                 prediction = model(image_tensor)
 
@@ -40,31 +48,45 @@ def perform_object_detection(img_folder, model_weights_path, show_image=True, sa
             labels = prediction[0]['labels'].cpu().numpy()
             scores = prediction[0]['scores'].cpu().numpy()
 
-            # Visualizar los resultados
-            fig, ax = plt.subplots(1, figsize=(8, 6))
-
-            ax.imshow(image)  # Mostrar imagen RGB
+            result = {
+                'image_filename': image_filename,
+                'detections': []
+            }
 
             for box, label, score in zip(boxes, labels, scores):
                 if score > probabilidad:
                     x, y, x_max, y_max = box
                     w, h = x_max - x, y_max - y
-                    rect = plt.Rectangle(
-                        (x, y), w, h, fill=False, color='red', linewidth=2)
-                    ax.add_patch(rect)
-                    ax.text(x, y, f"{classes[label]}: {score:.2f}", color='red')
+                    detection_info = {
+                        'box': [x, y, w, h],
+                        'label': classes[label],
+                        'score': score
+                    }
+                    result['detections'].append(detection_info)
 
+            detection_results.append(result)
 
             if save_path:
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
+                save_filename = os.path.splitext(image_filename)[0] + '_processed.jpg'
+                save_image_path = os.path.join(save_path, save_filename)
+                plt.imshow(image)
+                ax = plt.gca()
 
-            save_filename = os.path.splitext(image_filename)[0] + '_processed.jpg'
-            save_image_path = os.path.join(save_path, save_filename)
-            plt.savefig(save_image_path)                
+                for detection in result['detections']:
+                    x, y, w, h = detection['box']
+                    rect = plt.Rectangle(
+                        (x, y), w, h, fill=False, color='red', linewidth=2)
+                    ax.add_patch(rect)
+                    ax.text(x, y, f"{detection['label']}: {detection['score']:.2f}", color='red')
 
-            if show_image:
-                plt.show()
+                plt.savefig(save_image_path)
+                save_detection_results_to_json(json_path, image_filename, detection_results)
+
+                if show_image:
+                    plt.show()
+                # plt.clf()
 
 # Si este módulo se ejecuta directamente
 if __name__ == "__main__":
@@ -72,5 +94,6 @@ if __name__ == "__main__":
     model_weights_path = './propio/modelo_entrenado.pth'
     show_image = False
     save_path = './propio/rosasPost'
-    perform_object_detection(img_folder, model_weights_path, show_image, save_path)   
+    json_path = './propio/rosasPost'
+    perform_object_detection(img_folder, model_weights_path, show_image, save_path, json_path)   
                 
